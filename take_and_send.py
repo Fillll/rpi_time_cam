6b071cbfd0a4fa6c5f23b129bbd67d9f2fde48ec
FIL <git@fillll.ru>
2017-07-15 09:28:34 +0300

    As a class.

--- take_and_send.py
+++ take_and_send.py
@@ -5,24 +5,39 @@ import telepot
 import yaml
 
 
-def take_pic(filename):
-    camera = picamera.PiCamera()
-    camera.resolution = (3280, 2464)
-    camera.rotation = 180
-    camera.capture(filename)
+class TakeAndSend(object):
+    def __init__(self, config_filename):
+        super(TakeAndSend, self).__init__()
+        self.config_filename = config_filename
+        with open(config_filename) as config_file:
+            self.config = yaml.load(config_file.read())
+        self._init_camera()
+        self._init_tg()
+        time.sleep(self.config['init_sleep'])
 
+    def _init_camera(self):
+        self.camera = picamera.PiCamera()
+        self.camera.resolution = (self.config['camera_resolution_x'], self.config['camera_resolution_y'])
+        self.camera.rotation = self.config['camera_rotaion']
 
-def send_to_tg(filename):
-    with open('/home/pi/rpi_time_cam/config.yml') as config_file:
-        config = yaml.load(config_file.read())
-    tg_bot = telepot.Bot(config['telegram_token'])
-    text = time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime())
-    f = open(filename, 'rb')
-    tg_bot.sendPhoto(config['channel_id'], f, caption=text, disable_notification=True)
-    f.close()
+    def _init_tg(self):
+        self.tg_bot = telepot.Bot(self.config['telegram_token'])
+
+    def take_pic(self):
+        self.camera.capture(self.config['camera_pic_filename'])
+
+    def send_pic(self):
+        with open(self.config['camera_pic_filename'], 'rb') as picture:
+            text = time.strftime('%Y-%m-%d %H:%M:%S %Z', time.gmtime())
+            self.tg_bot.sendPhoto(self.config['channel_id'], picture, caption=text, disable_notification=True)
 
 
 if __name__ == '__main__':
-    filename = '/tmp/test.jpg'
-    take_pic(filename)
-    send_to_tg(filename)
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config', default='config.yml')
+    args = parser.parse_args()
+
+    take_n_send = TakeAndSend(args.config)
+    take_n_send.take_pic()
+    take_n_send.send_pic()
